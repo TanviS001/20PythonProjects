eb5d8588845719c4660bfb49e294fcbfb47e5abe
TanviSuryavanshi <145559687+TanviSuryavanshi@users.noreply.github.com>
2023-09-20 16:29:22 +0530

    Create EmailSender.py

---
+++ EmailSender.py
@@ -0,0 +1,28 @@
+from email.message import EmailMessage
+import smtplib
+import ssl
+
+email_sender = "tanvisuryavanshi01@gmail.com"
+email_password = "nolbnwznlhbjzgdt"
+
+email_receiver = "tanishqas001@gmail.com"
+
+subject = "Great thing!"
+body = """
+Hey there, 
+    Just created an EmailSender with Python.
+"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['Subject'] = subject
+em.set_content(body)
+
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
+  
