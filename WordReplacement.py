24d99b0bbdfec618b3af44827ff66099ea35763d
TanviSuryavanshi <145559687+TanviSuryavanshi@users.noreply.github.com>
2023-09-20 16:36:24 +0530

    Create WordReplacement.py

---
+++ WordReplacement.py
@@ -0,0 +1,8 @@
+def replace_word():
+    text = " "
+    word_to_replace = input("Enter the word you want to replace: ")
+    word_replacement = input("Enter the replacement: ")
+    print(text.replace(word_to_replace, word_replacement))
+
+
+replace_word()
