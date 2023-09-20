d2eb9224c034a876c650e16bb6e627247b05cb55
TanviSuryavanshi <145559687+TanviSuryavanshi@users.noreply.github.com>
2023-09-20 16:34:07 +0530

    Create Madlibs.py

---
+++ Madlibs.py
@@ -0,0 +1,71 @@
+# Mad-libs
+
+print("Let's play Mad-libs!!")
+print("""
+Mad-libs is a word game where players create a story 
+by filling in missing words in a given paragraph or template. 
+""")
+print("/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////")
+
+# Line 1
+place101 = input("Enter place: ")
+ad102 = input("Enter an adjective: ")
+noun103 = input("Enter a noun: ")
+name104 = input("Enter a name: ")
+
+# Line 2
+verb201 = input("Enter a verb ending in -ing: ")
+noun202 = input("enter a noun: ")
+name203 = input("Enter a name: ")
+noun204 = input("Enter a noun: ")
+
+# Line 3
+name301 = input("Enter a name: ")
+verb302 = input("enter a verb: ")
+
+# Line 4
+name401 = input("Enter a name: ")
+noun402 = input("Enter a noun: ")
+
+# Line 5
+ad501 = input("Enter an adjective: ")
+
+print("10 More!")
+
+# Line 6
+name601 = input("Enter a name: ")
+noun602 = input("Enter a plural noun: ")
+animal603 = input("Enter an animal: ")
+noun604 = input("Enter a noun: ")
+
+# Line 7
+ad701 = input("Enter an adjective: ")
+
+print("Just 5 more!")
+
+# Line 8
+name801 = input("Enter a name: ")
+verb802 = input("Enter a verb ending with ing: ")
+noun803 = input("Enter a noun: ")
+noun804 = input("Enter a plural noun: ")
+
+print("Just one more!")
+
+# Line 9
+name901 = input("Enter a name: ")
+
+
+print("Your Story...")
+
+print(f"""
+Once upon a time in the whimsical land of {place101}, there was a {ad102} {noun103} named {name104}.
+One day, while {verb201} in the {noun202}, {name203} stumbled upon a mysterious {noun204}.
+Curiosity sparked within {name301}, and they decided to {verb302} it open.
+To their astonishment, {name401} discovered a portal to a magical {noun402}
+Excitement filled their heart as they bravely stepped through the portal, into a world filled with {ad501} wonders.
+In this enchanted realm, {name601} encountered talking {noun602}, friendly {animal603}, and even a wise old {noun604}.
+Together, they embarked on a(n) {ad701} adventure, facing challenges and overcoming obstacles.
+Along the way, {name801} learned valuable lessons about {verb802}, {noun803}, and the power of {noun804}.
+Finally, after an exhilarating journey, {name901} found themselves back in their own world, forever changed by the incredible experience.
+They cherished the memories and carried the magic of the adventure in their heart, always longing for the next extraordinary chapter in their life.
+""")
